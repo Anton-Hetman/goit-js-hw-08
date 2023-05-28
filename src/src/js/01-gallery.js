f2f62a3cbc994614e5669d6f00908e6e50ee4514
Anton Hetman <00kyp1d0n41k00@gmail.com>
2023-05-28 21:19:03 +0300

    21:15

---
+++ src/src/js/01-gallery.js
@@ -0,0 +1,48 @@
+// Add imports above this line
+import { galleryItems } from "./gallery-items";
+// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const allGallery = document.querySelector(".gallery");
+const galleryList = galleryItems
+  .map(
+    ({ original, preview, description }) =>
+      `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+allGallery.innerHTML = galleryList;
+
+allGallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return console.log("not img");
+  }
+  const eventSource = e.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`
+    <img src="${eventSource}" width="800" height="600">
+`);
+  instance.show();
+
+  allGallery.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+});
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+console.log(galleryItems);
---
+++ src/src/js/02-video.js
@@ -0,0 +1,15 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const currentTime = "videoplayer-current-time";
+
+const iframe = document.querySelector("iframe");
+
+const iframePlayer = new Player(iframe, {});
+iframePlayer.setCurrentTime(localStorage.getItem(currentTime));
+iframePlayer.on(
+  "timeupdate",
+  throttle(function (e) {
+    localStorage.setItem(currentTime, e.seconds);
+  }, 1000)
+);
