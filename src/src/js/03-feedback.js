f00484047550fb353c4f38e13325db547eb89cb5
Anton Hetman <00kyp1d0n41k00@gmail.com>
2023-06-01 09:33:05 +0300

    the end

--- src/src/js/03-feedback.js
+++ src/src/js/03-feedback.js
@@ -1,23 +1,23 @@
-import throttle from "lodash.throttle";
+import throttle from 'lodash.throttle';
 
-const formElem = document.querySelector(".feedback-form");
-const emailElem = document.querySelector(".feedback-form input");
-const textareaElem = document.querySelector(".feedback-form textarea");
+const formElem = document.querySelector('.feedback-form');
+const emailElem = document.querySelector('.feedback-form input');
+const textareaElem = document.querySelector('.feedback-form textarea');
 
-const currentTime = "feedback-form-state";
+const currentTime = 'feedback-form-state';
 const saveForm = localStorage.getItem(currentTime);
 let formData = {};
 
-formElem.addEventListener("input", throttle(formInput, 500));
-formElem.addEventListener("submit", formSubmit);
+formElem.addEventListener('input', throttle(formInput, 500));
+formElem.addEventListener('submit', formSubmit);
 
 pasteFormData();
 
 function pasteFormData(e) {
   if (saveForm) {
     formData = JSON.parse(saveForm);
-    emailElem.value = formData.email || 0;
-    textareaElem.value = formData.message || 0;
+    emailElem.value = formData.email || '';
+    textareaElem.value = formData.message || '';
   }
 }
 
